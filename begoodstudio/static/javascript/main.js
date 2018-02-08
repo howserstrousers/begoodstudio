f1c65d51e4240dff27810567801efdd6eb8d4e27
howserstrousers <fritzcharleshowser@gmail.com>
2018-02-07 20:32:03 -0500

    sliders working

--- begoodstudio/static/javascript/main.js
+++ begoodstudio/static/javascript/main.js
@@ -1,16 +1,15 @@
-window.onload = function() {
+
 	$(document).ready(function() {
-			$('.carousel_container').slick({
-              arrows:false,
-              adaptiveHeight:true,
-            });
+		$('.carousel_container').slick({
+        arrows:false,
+        adaptiveHeight:true,
+      });
 
-            $('.previous').click(function(){
-              $('.carousel_container').slick('slickPrev');
-            });
+      $('.previous').click(function(){
+        $('.carousel_container').slick('slickPrev');
+      });
 
-            $('.next').click(function(){
-              $('.carousel_container').slick('slickNext');
-            });
-	}
-}
+      $('.next').click(function(){
+        $('.carousel_container').slick('slickNext');
+    });
+	});
