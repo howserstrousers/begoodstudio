632fb3c058c31ed8fa13e5063400052faf538686
howserstrousers <fritzcharleshowser@gmail.com>
2018-04-16 19:02:12 -0400

    mobile nav

--- begoodstudio/static/javascript/main.js
+++ begoodstudio/static/javascript/main.js
@@ -1,143 +1,153 @@
 $(document).ready(function() {
-
-    $('.carousel_container').each(function (idx, item) {
-        var carouselId = "carousel" + idx;
-        this.id = carouselId;
-        $(this).slick({
-            slide: "#" + carouselId +" .carousel_image",
-            appendArrows: "#" + carouselId + " .prev_next",
-            prevArrow: '<a>&#8249</a>',
-            nextArrow: '<a>&#8250</a>',
+    //Carousel
+        $('.carousel_container').each(function (idx, item) {
+            var carouselId = "carousel" + idx;
+            this.id = carouselId;
+            $(this).slick({
+                slide: "#" + carouselId +" .carousel_image",
+                appendArrows: "#" + carouselId + " .prev_next",
+                prevArrow: '<a>&#8249</a>',
+                nextArrow: '<a>&#8250</a>',
+                lazyLoad: 'progressive',
+            });
         });
-    });
 
-    function setHeight() {
-      windowHeight = $(window).innerHeight();
-      $('#maincontainer').css('min-height', windowHeight);
-    };
-    setHeight();
-    
-    $(window).resize(function() {
+        function setHeight() {
+          windowHeight = $(window).innerHeight();
+          $('#maincontainer').css('min-height', windowHeight);
+        };
         setHeight();
-    });
-//Main container and close
-    $('#projectlist a').click(function(){
-        $('#maincontainer, #ec1').css({
-            display: 'block'
+        
+        $(window).resize(function() {
+            setHeight();
         });
-        $("#maincontainer").load($(this).attr('href'));
-         return false;
-    });
+    // END Carousel
+});
 
-    $('#ec1, #abouttrigger a, #contacttrigger a').click(function(){
-        $('#maincontainer, #ec1').css({
-            display: 'none'
-        });
-    });
+//DESKTOP JAVASCRIPT   
+if ( $(window).width() > 769) {
 
-    $('#contacttrigger a').click(function(){
-        $('#aboutcontainer, #ec2').css({
-            display: 'none'
-        });
-    });
-
-    $('#abouttrigger a').click(function(){
-        $('#contactcontainer, #ec3').css({
-            display: 'none'
-        });
-    });
-
-    $('#cstrigger a').click(function(){
-        $('#aboutcontainer, #ec2, #contactcontainer, #ec3').css({
-            display: 'none'
-        });
-    });
-
-
-//nav color change
-    $('.headercontainer a').click( function() { 
-          $('#cstrigger, #abouttrigger, #contacttrigger').css("color", "black");
-          $(this).css("color", "white");
-    });
-
-    $('#abouttrigger').click( function() { 
-          $('#cstrigger, #contacttrigger').css("color", "black");
-          $(this).css("color", "white");
-    });
-
-//project list move to bg
-    $('#projectlist a').click(function(){
-        $('#projectlist a').css({
-            position: 'static' 
-        });
-    });
-    
-//Case studies Main container and close
-    $('#cstrigger a').click(function(){
-        $('#maincontainer , #ec1').css({
-            display: 'block'
-        });
+    //Main container and close
+        $('#projectlist a').click(function(){
+            $('#maincontainer, #ec1').css({
+                display: 'block'
+            });
 
         $("#maincontainer").load($(this).attr('href'));
-         return false;
-    });
-
-    $('.cslist a').click(function(){
-        $('#maincontainer , #ec1').css({
-            display: 'block'
-        });
-        $("#maincontainer").load($(this).attr('href'));
-         return false;
-    });
-
-    $('#cstrigger a').click(function(){
-        $('#projectlist a').css({
-            position: 'static'
-        });
-    });
-
-//About container and close
-    $('#abouttrigger a').click(function(){
-        $('#aboutcontainer, #ec2').css({
-            display: 'block'
-        });
-        $("#aboutcontainer").load($(this).attr('href'));
-         return false;
-    });
-
-    $('#ec2').click(function(){
-        $('#aboutcontainer, #ec2').css({
-            display: 'none'
-        });
-    });
-
-    $('#abouttrigger a').click(function(){
-        $('#projectlist a').css({
-            position: 'static'
-        });
-    });
-
-//Contact container and close
-    $('#contacttrigger a').click(function(){
-        $('#contactcontainer, #ec3').css({
-            display: 'block'
+             return false;
         });
 
-        $("#contactcontainer").load($(this).attr('href'));
-         return false;
-    });
-
-    $('#ec3').click(function(){
-        $('#contactcontainer, #ec3').css({
-            display: 'none'
+        $('#ec1, #abouttrigger a, #contacttrigger a').click(function(){
+            $('#maincontainer, #ec1').css({
+                display: 'none'
+            });
         });
-    });
 
-    $('#contacttrigger a').click(function(){
-        $('#projectlist a').css({
-            position: 'static'
+        $('#contacttrigger a').click(function(){
+            $('#aboutcontainer, #ec2').css({
+                display: 'none'
+            });
         });
-    });
+
+        $('#abouttrigger a').click(function(){
+            $('#contactcontainer, #ec3').css({
+                display: 'none'
+            });
+        });
+
+        $('#cstrigger a').click(function(){
+            $('#aboutcontainer, #ec2, #contactcontainer, #ec3').css({
+                display: 'none'
+            });
+        });
+
+
+    /*nav color change
+        $('.headercontainer a').click( function() { 
+              $('#cstrigger, #abouttrigger, #contacttrigger').css("color", "black");
+              $(this).css("color", "white");
+        });
+
+        $('#abouttrigger').click( function() { 
+              $('#cstrigger, #contacttrigger').css("color", "black");
+              $(this).css("color", "white");
+        });
+    */
+
+    //project list move to bg
+        $('#projectlist a').click(function(){
+            $('#projectlist a').css({
+                position: 'static' 
+            });
+        });
+        
+    //Case studies Main container and close
+        $('#cstrigger a').click(function(){
+            $('#maincontainer , #ec1').css({
+                display: 'block'
+            });
+
+            $("#maincontainer").load($(this).attr('href'));
+             return false;
+        });
+
+        $('.cslist a').click(function(){
+            $('#maincontainer , #ec1').css({
+                display: 'block'
+            });
+            $("#maincontainer").load($(this).attr('href'));
+             return false;
+        });
+
+        $('#cstrigger a').click(function(){
+            $('#projectlist a').css({
+                position: 'static'
+            });
+        });
+
+    //About container and close
+        $('#abouttrigger a').click(function(){
+            $('#aboutcontainer, #ec2').css({
+                display: 'block'
+            });
+            $("#aboutcontainer").load($(this).attr('href'));
+             return false;
+        });
+
+        $('#ec2').click(function(){
+            $('#aboutcontainer, #ec2').css({
+                display: 'none'
+            });
+        });
+
+        $('#abouttrigger a').click(function(){
+            $('#projectlist a').css({
+                position: 'static'
+            });
+        });
+
+    //Contact container and close
+        $('#contacttrigger a').click(function(){
+            $('#contactcontainer, #ec3').css({
+                display: 'block'
+            });
+
+            $("#contactcontainer").load($(this).attr('href'));
+             return false;
+        });
+
+        $('#ec3').click(function(){
+            $('#contactcontainer, #ec3').css({
+                display: 'none'
+            });
+        });
+
+        $('#contacttrigger a').click(function(){
+            $('#projectlist a').css({
+                position: 'static'
+            });
+        });
+
 
 //URL load
     var setCurrentPage = function(url) {
@@ -158,18 +168,17 @@ $(document).ready(function() {
 
     window.onpopstate = function(e) {
         $("#menu-nav a").fadeTo('fast', 1.0);
-        setCurrentPage(e.state ? e.state.url : null);
+            setCurrentPage(e.state ? e.state.url : null);
     };
-  });
+}
+//END DESKTOP JAVASCRIPT
 
 //Main container fixed
-
     var fixed = document.getElementById('projectlist');
-    fixed.addEventListener('touchmove', function(e) {
-            e.preventDefault();
-    }, false);
-
-//Lazy load
-    $(function() {
-        $('.lazy').Lazy();
+    $('#projectlist a').click(function(){
+        fixed.addEventListener('touchmove', function(e) {
+                e.preventDefault();
+        }, false);
     });
+
+
